ac3e1be87da31bfef30f691272fa70182b30d982
Pablo Aquino <pabloaquino1988@gmail.com>
2024-01-03 18:34:22 -0300

    static index.html added

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,14 +1,14 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class DemoController {
     @CrossOrigin(origins = "*")
     @GetMapping("/")
     public String hello() {
-        return "Hello from Render";
+        return "index.html";
     }
 }
