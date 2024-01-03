fdac328f3868e5aae28da958b64538937f27a266
Pablo Aquino <pabloaquino1988@gmail.com>
2024-01-03 19:05:06 -0300

    @RestController annotation added and return a String

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,14 +1,14 @@
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class DemoController {
     @CrossOrigin(origins = "*")
     @GetMapping("/")
     public String hello() {
-        return "index.html";
+        return "Hello from Render";
     }
 }
